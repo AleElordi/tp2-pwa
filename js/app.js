7ea6e432beb8ab005dcba88f1631ade82e380bbe
AleElordi <81270688+AleElordi@users.noreply.github.com>
2022-06-01 23:04:23 -0300

    Agrego el listado de pokemones

--- js/app.js
+++ js/app.js
@@ -11,6 +11,54 @@ const d = document;
 const button = document.getElementById('sendButton');
 const inputElement = document.getElementById('search');
 const resDiv = d.querySelector('#pokeResultado');
+const listDiv = d.querySelector('#pokeLista');
+
+
+
+//-----------------------Busqueda listado completo------------------------//
+
+const gqlVariables = {
+  limit: 2,
+  offset: 1,
+};
+
+const gqlQuery = `query pokemons($limit: Int, $offset: Int) {
+  pokemons(limit: ${gqlVariables.limit}, offset: ${gqlVariables.offset}) {
+    count
+    next
+    previous
+    status
+    message
+    results {
+      url
+      name
+      image
+    }
+  }
+}`;
+
+fetch('https://graphql-pokeapi.graphcdn.app/', {
+  credentials: 'omit',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    query: gqlQuery,
+    variables: gqlVariables,
+  }),
+  method: 'POST',
+})
+.then(function(json){
+  listDiv.innerHTML = JSON.stringify(json.data)
+})
+.finally(function(){
+  //spiner de loading
+})
+.catch(function(err){
+  console.log('Error',err)
+})
+
+
+//-----------------------Busqueda individial------------------------//
+
 
 const pokeQueryBusqueda = (pokemon) => `query pokemon {
   pokemon(name:"${pokemon}") {
@@ -31,6 +79,7 @@ const pokeQueryBusqueda = (pokemon) => `query pokemon {
     }
   }
 }`
+
 
 button.addEventListener("click", ()=> {
 
